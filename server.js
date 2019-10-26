8f44c0693a79825b94677b39fccd7db3285b8757
Kevin Malazarte <Kevin.Malazarte@gmail.com>
2019-10-26 10:47:39 -0400

    added demo file

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 let http = require('http');
 let dt = require('./myfirstmodule');
+let url = require('url');
+let fs = require('fs');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end("The date and time are currently: " + dt.myDateTime());
+  fs.readFile('demofile.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    // res.write(req.url);
+  // let q = url.parse(req.url, true).query;
+  // let txt = q.year + " " + q.month;
+  // res.end("The date and time are currently: " + dt.myDateTime());
+    res.end()
+  })
 }).listen(8080);
